c99e23a985e3eeb4f2bdf9133a75f023ae4896f3
Ayano NARA <ayano@AyanonoMacBook-Air.local>
2021-11-28 20:57:42 +0900

    アラートボタンを追加した

---
+++ index.js
@@ -0,0 +1,8 @@
+function clickButton() {
+    const bast = 100;
+    const hip = 200;
+    let west = 300;
+    const amount = bast + hip + west;
+    const message = '私のbhwの合計' + amount + 'です';
+    window.alert(message);
+}
